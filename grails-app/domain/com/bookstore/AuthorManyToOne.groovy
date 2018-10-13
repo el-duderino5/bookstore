3578c68d7d288143363f923bb180472bdaeb8839
Candido Pichardo <gregorip@gmail.com>
2018-10-13 11:17:35 -0400

    Create unidirectional and bidirectional relationships between AuthorManyToOne and BookManyToOne

--- grails-app/domain/com/bookstore/AuthorManyToOne.groovy
+++ grails-app/domain/com/bookstore/AuthorManyToOne.groovy
@@ -3,6 +3,7 @@ package com.bookstore
 class AuthorManyToOne {
 
     String name
+    BookManyToOne book
 
     static constraints = {
     }
--- grails-app/domain/com/bookstore/BookManyToOne.groovy
+++ grails-app/domain/com/bookstore/BookManyToOne.groovy
@@ -4,6 +4,10 @@ class BookManyToOne {
 
     String title
 
+//    static belongsTo = AuthorManyToOne //TODO: enable this line to turn the relationship between AuthorManyToOne and BookManyToOne unidirectional
+    static belongsTo = [author: AuthorManyToOne]
+    // TODO: enable this line to turn the relationship between AuthorManyToOne and BookManyToOne bidirectional
+
     static constraints = {
     }
 }
